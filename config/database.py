1c35b0f264667f1fe0de53913c497784e04859be
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-01-27 15:50:50 -0300

    feat: cria diretorio das configuracoes da aplicacao

---
+++ config/database.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = quote_plus(os.getenv("DB_USERNAME"))
+password = quote_plus(os.getenv("DB_PASSWORD"))
+
+client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.auyee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+db = client.bm_db
+
+users_collection = db["users"]
+offered_services_collection = db["offeredServices"]
