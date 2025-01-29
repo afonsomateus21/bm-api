517a88e624caae105d04859a59bc52b513fbf75d
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-01-29 01:04:56 -0300

    feat: implementa autenticacao com o google

--- routers/user/controllers.py
+++ routers/user/controllers.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Request, HTTPException
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 from starlette.requests import Request
 from starlette import status
 from .models import User
-from .services import bcrypt_context, authenticate_user, create_access_token, list_serial, user_dependency, create_refresh_token, token_expired, decode_token, check_if_user_already_exits
-from .validators import CreateUserRequest, UpdateUserRequest, Token, RefreshTokenRequest, LoginRequest, UserType
+from .services import bcrypt_context, individual_serial, create_user_from_google_info, authenticate_user, get_user_by_google_sub, create_access_token, list_serial, user_dependency, create_refresh_token, token_expired, decode_token, check_if_user_already_exits
+from .validators import CreateUserRequest, UpdateUserRequest, Token, RefreshTokenRequest, LoginRequest, UserType, GoogleUser
 from config.database import users_collection
 from datetime import timedelta
 from bson import ObjectId
@@ -36,16 +36,36 @@ async def login_google(request: Request):
   return await oauth.google.authorize_redirect(request, GOOGLE_REDIRECT_URI)
 
 @auth_router.get("/google/callback")
-async def auth_callback(request: Request):
+async def auth_google(request: Request):
   try:
-    print("entrou auth_callback")
-    user_info = await oauth.google.authorize_access_token(request)
-    print("user_info:", user_info) 
-
-    return user_info
-        
+    user_response = await oauth.google.authorize_access_token(request)
   except Exception as e:
-    raise HTTPException(status_code=400, detail=str(e))
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+        
+  user_info = user_response.get("userinfo")
+
+  print(user_info)
+
+  google_user = GoogleUser(
+    sub=user_info.get("sub"),
+    email=user_info.get("email"),
+    name=user_info.get("given_name")
+  )
+
+  existing_user = get_user_by_google_sub(google_user.sub)
+
+  if existing_user:
+    print("Existing user")
+    user = existing_user
+  else:
+    print("Creating user")
+    user = create_user_from_google_info(google_user)
+    print("user created: ", user)
+
+  access_token = create_access_token(user.email, user.id, timedelta(days=7))
+  refresh_token = create_refresh_token(user.email, user.id, timedelta(days=14))
+
+  return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"} 
   
 @auth_router.post("/user/customer", status_code=status.HTTP_201_CREATED)
 async def create_customer(create_user_request: CreateUserRequest):
@@ -60,9 +80,11 @@ async def create_customer(create_user_request: CreateUserRequest):
   if (check_if_user_already_exits(create_user_model.email)):
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists.")
 
-  users_collection.insert_one(dict(create_user_model))
+  result = users_collection.insert_one(create_user_model.to_dict())
 
-  return create_user_request
+  user_created = users_collection.find_one({ "_id": result.inserted_id })
+
+  return individual_serial(user_created)
 
 @auth_router.post("/user/admin", status_code=status.HTTP_201_CREATED)
 async def create_admin(create_user_request: CreateUserRequest, current_user: user_dependency):
@@ -80,9 +102,11 @@ async def create_admin(create_user_request: CreateUserRequest, current_user: use
   if (check_if_user_already_exits(create_user_model.email)):
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists.")
 
-  users_collection.insert_one(dict(create_user_model))
+  result = users_collection.insert_one(create_user_model.to_dict())
 
-  return create_user_request.model_dump(exclude={"password"})
+  user_created = users_collection.find_one({ "_id": result.inserted_id })
+
+  return individual_serial(user_created)
 
 @auth_router.get("/user/admin/{id}", status_code=status.HTTP_200_OK)
 async def get_admin(id: str, current_user: user_dependency):
