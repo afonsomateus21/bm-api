0c7487a36eca2a1b72666c0f6a1a5c5fb0799da6
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-13 22:07:00 -0300

    feat: adiciona regra de CORS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers.user.controllers import auth_router
 from routers.offered_services.controllers import offered_services_router
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
@@ -9,6 +10,14 @@ app = FastAPI()
 app.add_middleware(
   SessionMiddleware,
   secret_key=os.getenv("SECRET_KEY"), 
+)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix="/api")
