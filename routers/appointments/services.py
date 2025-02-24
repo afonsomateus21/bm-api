4aeb33b20a73c687accdedf8bde883fb65d63d38
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-23 22:59:57 -0300

    feat: adiciona funcao para notificar usuario sobre novo agendamento

--- routers/appointments/services.py
+++ routers/appointments/services.py
@@ -1,6 +1,11 @@
 import datetime
 from config.database import appointments_collection, users_collection, offered_services_collection
+from config.email import api_instance
 from bson import ObjectId
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from pprint import pprint
+
 
 def check_if_date_and_hour_are_available(date: str, hour:int, professional_id: str, customer_id: str):
     professional_object_id = ObjectId(professional_id)
@@ -30,6 +35,19 @@ def get_service_for_appointment(id: str):
   })
 
   return service
+
+def notify_new_appointment(to_email: str, to_name: str, subject: str, html_content: str):
+  email_data = sib_api_v3_sdk.SendSmtpEmail(
+    to=[{"email": to_email, "name": to_name}],
+    sender={"email": "afonsomateus.dev@gmail.com", "name": "Equipe Beauty Manager"},
+    subject=subject,
+    html_content=html_content
+  )
+  try:
+    api_instance.send_transac_email(email_data)
+    return {"message": "E-mail enviado com sucesso!"}
+  except ApiException as e:
+    print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
 
 
 def serialize_appointment(appointment):
@@ -63,7 +81,6 @@ def individual_serial(appointment) -> dict:
     "hour": str(appointment["hour"]),
     "is_notifiable": bool(appointment["is_notifiable"])
   }
-
 
 def list_serial(appointments) -> list:
   return [individual_serial(appointment) for appointment in appointments]
