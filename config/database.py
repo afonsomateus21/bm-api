ecefd20f889534777607b132f16eaa55e1ab4976
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-24 13:02:45 -0300

    feat: remove collection de reserva

--- config/database.py
+++ config/database.py
@@ -15,4 +15,3 @@ db = client.bm_db
 users_collection = db["users"]
 offered_services_collection = db["offeredServices"]
 appointments_collection = db["appointments"]
-reservations = db["reservations"]
